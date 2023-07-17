2171c7833fd2c91fc81503d75513b6f879548468
why-not-available <aever144@naver.com>
2023-07-17 14:05:37 +0900

    발버둥

--- src/main/java/com/bubble/welcomepet/community/controller/AjaxRestControllerForCommunity.java
+++ src/main/java/com/bubble/welcomepet/community/controller/AjaxRestControllerForCommunity.java
@@ -1,5 +1,8 @@
 package com.bubble.welcomepet.community.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bubble.welcomepet.community.service.CommunityServiceImpl;
 
 @RestController
-@RequestMapping()
+@RequestMapping("bringCommentByPostNo")
 public class AjaxRestControllerForCommunity {
 	@Autowired
 	private CommunityServiceImpl communityServiceImpl;
 	
-//	@RequestMapping()
-
+	@RequestMapping("getCommentList")
+	public Map<String, Object> getCommentListAjax(int show_dog_post_no){
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("commentList", communityServiceImpl.bringCommentByPostNo(show_dog_post_no));
+		return map;
+	}
 }
